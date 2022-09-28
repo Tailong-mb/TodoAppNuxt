6e2ca1bd94d635ab29481372554132f2ed8ed677
Michael Bardou <michael.bardou@icloud.com>
2022-09-28 09:57:55 +0200

    finish

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from "nuxt3";
 
-})
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui"],
+});
